64bed13f0428c8e7182f84a21dfb79287803231f
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-14 02:10:01 +0530

    add break points to PageNotFound.tsx

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,8 +1,8 @@
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center  bg-gradient-to-r from-black to-violet-900 min-h-screen">
+    <div className="bg-gradient-to-r from-black to-violet-900 min-h-screen flex justify-between items-center text-center sm:text-start sm:flex-col sm:justify-center sm:items-center">
       <div className="text-white space-y-1">
-        <div className="text-5xl font-bold text-stone-100">
+        <div className="font-bold text-stone-100 text-3xl md:text-4xl lg:text-5xl">
           These are uncharted waters
         </div>
         <div className="text-lg text-pink-400">
