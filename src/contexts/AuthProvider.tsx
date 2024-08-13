dc136fed5bcc3c963ae866d74d55a760f19972c5
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-14 00:00:27 +0530

    feat: create AuthProvider.tsx and useAuth.ts

---
+++ src/contexts/AuthProvider.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebase";
+
+export interface AuthContextType {
+  user: User | null;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined,
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
